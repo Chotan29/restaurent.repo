b7cb68d5e72dc7f9fb7920846813ca9d2790b823
Chotan Chandra Dash <chotan1duet@gmail.com>
2024-01-21 13:43:04 +0600

    Day52 React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import MainComponent from './mainComponent/MainComponent';
+
+function App() {
+  return (
+    <div>
+      <MainComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ src/mainComponent/MainComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Body from './body/Body';
+import Header from './header/Header';
+import Footer from './footer/footer';
+
+const MainComponent = () => {
+    return (
+        <>
+            <Header />
+            <Body />
+            <Footer />
+        </>
+    )
+}
+export default MainComponent;
