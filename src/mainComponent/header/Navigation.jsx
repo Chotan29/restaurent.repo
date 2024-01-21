27c78e6298799856d1c7792daa812a4ada6995c0
Chotan Chandra Dash <chotan1duet@gmail.com>
2024-01-21 13:57:57 +0600

    Day52 React 2nd commit

---
+++ src/mainComponent/header/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+const Navigation = () => {
+    return (
+        <>
+            <Navbar dark color='dark'>
+                <NavbarBrand>AIT Resturent</NavbarBrand>
+            </Navbar>
+        </>
+    )
+}
+export default Navigation;
